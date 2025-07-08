324ecb3f13a050b6ce89a23f07a2b2195be623c9
Amr Abobakr Ahmed <amrhassan666777@gmail.com>
2025-07-08 21:00:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,70 @@
+
+//Q1
+import java.sql.SQLOutput;
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner numm = new Scanner(System.in) ;
+//System.out.print("Enter first num: ");
+//            double num1 = numm.nextDouble();
+//        System.out.print("Enter second num: ");
+//        double num2 = numm.nextDouble();
+//        System.out.println("Sum = " +(num1+num2));
+//        System.out.println("Differance = " +(num1-num2));
+//        System.out.println("Product = " +(num1*num2));
+//        System.out.println("Quotient = " +(num1/num2));
+//    }
+
+//Q2
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner user=new Scanner(System.in);
+//        System.out.println("Write a sentence: ");
+//        String sentence= user.nextLine();
+//        String upper_case=sentence.toUpperCase();
+//        int char_count=sentence.length();
+//        char first_char= sentence.charAt(0);
+//        System.out.println("Uppercase: "+ upper_case);
+//        System.out.println("Length: "+ char_count);
+//        System.out.println("First character: "+ first_char);
+//    }}
+
+
+//Q3
+//import java.util.Scanner;
+//public class Main {
+//  public static void main(String[] args) {
+//                Scanner input = new Scanner(System.in);
+//                System.out.print("Enter your age: ");
+//                int age = input.nextInt();
+//              input.nextLine();
+//                System.out.print("Enter your nationality: ");
+//                String nationality = input.next();
+//                if (age >= 18 && nationality.equals("Egyptian")) {
+//                    System.out.println("Eligible to vote");
+//                } else {
+//                    System.out.println("Not eligible");
+//                }
+//            }
+//        }
+
+
+
+//Q4
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner input = new Scanner (System.in);
+//        System.out.println("Enter number: ");
+//        int num = input.nextInt() ;
+//        if (num %2==0){
+//            System.out.println("Even number");}
+//        else {
+//                System.out.println("Odd number");
+//            }
+//        }
+//        }
+
